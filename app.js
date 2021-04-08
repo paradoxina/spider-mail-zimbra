eaa47dbdffc6d2e9e20ce2460b2123622070ed1b
Aleksey <paradoxina@bk.ru>
2021-04-08 10:45:51 +0300

    first

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const conf = require("./config.json");
 
 (async () => {
     for (const user of conf.users) {
-        if (user.password !== "" && user.login !== "") {
+        if (user.password !== "" && user.login !== "" && conf.url !== "") {
             await browser.run(user.login, user.password, conf.url)
-        }
+        }else console.log("enter your username, password and url in config.json")
     }
 })()
 
